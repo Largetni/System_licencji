95120e6fc6b8876ef6c7381e0b6f8a121ae02742
Jakub Slominski <u002750@gmail.com>
2019-01-27 20:17:24 +0100

    added dbcontext options for appdbcontext

--- System do licencji/Models/AppDbContext.cs
+++ System do licencji/Models/AppDbContext.cs
@@ -10,7 +10,9 @@ namespace System_do_licencji.Models
 {
     public class AppDbContext : IdentityDbContext<IdentityUser>
     {
-       
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
 
     }
 }
