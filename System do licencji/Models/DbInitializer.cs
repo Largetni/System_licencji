bbdbf23885cfbfef19dde70d70e773ee57f3ca22
Jakub Slominski <u002750@gmail.com>
2019-01-27 20:09:40 +0100

    corrected project name

--- System do licencji/Models/DbInitializer.cs
+++ System do licencji/Models/DbInitializer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace System do licencji.Models
+namespace System_do_licencji.Models
 {
     public class DbInitializer
 {
