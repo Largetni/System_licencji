9d0c0e628eb700037ec47a5d7d34673a2e2f4daa
Jakub Slominski <u002750@gmail.com>
2019-01-28 21:17:39 +0100

    added player property structure

---
+++ System do licencji/Models/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace System_do_licencji.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public int ClubId { get; set; }
+        public bool HasFirstLicense { get; set; }
+        public bool HasSecondLicense { get; set; }
+        public bool HasThirdLicense { get; set; }
+        public bool LicenseIsRenewed { get; set; }
+   
+
+    }
+}
