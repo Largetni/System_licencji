b8c0102da57b80e30729241d49405775d85ae4b1
Jakub Slominski <u002750@gmail.com>
2019-01-28 21:23:26 +0100

    added repository interface

---
+++ System do licencji/Models/IPlayerRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace System_do_licencji.Models
+{
+    interface IPlayerRepository
+    {
+        IEnumerable<Player> GetAllPlayers();
+        Player GetPlayer(int playerId);
+
+    }
+}
