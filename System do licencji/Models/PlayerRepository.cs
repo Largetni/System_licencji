764de7942c1952440ae75634d0ed6d9ddaa93276
Jakub Slominski <u002750@gmail.com>
2019-01-28 21:24:50 +0100

    created player repository

---
+++ System do licencji/Models/PlayerRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace System_do_licencji.Models
+{
+    public class PlayerRepository : IPlayerRepository
+    {
+        public IEnumerable<Player> GetAllPlayers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Player GetPlayer(int playerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
