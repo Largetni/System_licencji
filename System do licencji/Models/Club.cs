bad0b08a96cae2d638a25d517fdbc738984b9be9
Jakub Slominski <u002750@gmail.com>
2019-02-04 19:39:19 +0100

    created class for clubs

---
+++ System do licencji/Models/Club.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace System_do_licencji.Models
+{
+    public class Club
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int OwnerId { get; set; }
+        public string Address { get; set; }
+        public int RegionId { get; set; }
+        public bool HasFirstLicense { get; set; }
+        public bool HasSecondLicense { get; set; }
+        public bool HasThirdLicense { get; set; }
+        public bool ClubLicenseIsRenewed { get; set; }
+    }
+}
