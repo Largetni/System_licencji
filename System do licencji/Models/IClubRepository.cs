f3185151d9a339a18685755ff0bd4568fc0604ae
Jakub Slominski <u002750@gmail.com>
2019-02-04 19:40:24 +0100

    created repository interface for clubs

---
+++ System do licencji/Models/IClubRepository.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace System_do_licencji.Models
+{
+    interface IClubRepository
+    {
+        IEnumerable<Club> GetAllClubs();
+        Club GetClubInfo(int clubId);
+
+    }
+}
