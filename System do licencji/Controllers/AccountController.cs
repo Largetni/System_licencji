74fe371738ee42ea562aea37eb05091ff9b04c6a
Jakub Slominski <u002750@gmail.com>
2019-02-04 19:36:34 +0100

    login and register implemented to controller

--- System do licencji/Controllers/AccountController.cs
+++ System do licencji/Controllers/AccountController.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System_do_licencji.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace System_do_licencji.Controllers
 {
     public class AccountController : Controller
-{
+    {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
 
@@ -20,10 +21,66 @@ namespace System_do_licencji.Controllers
             _userManager = userManager;
         }
 
-    // GET: /<controller>/
-    public IActionResult Index()
-    {
-        return View();
-    }
+        // GET: /<controller>/
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+
+            if (!ModelState.IsValid)
+                return View(loginVM);
+
+            var user = await _userManager.FindByNameAsync(loginVM.UserName);
+
+            if (user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+            }
+
+            ModelState.AddModelError("", "Nazwa użytkowninka lub hasło jest nieprawidłowe");
+
+            return View(loginVM);
+            
+        }
+
+        public IActionResult Register()
+        {
+            return View(new LoginVM());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(LoginVM loginVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser() { UserName = loginVM.UserName };
+                var result = await _userManager.CreateAsync(user, loginVM.Password);
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            return View(new LoginVM());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
 }
 }
