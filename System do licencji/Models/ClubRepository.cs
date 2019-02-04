03f39f7d406af761e1b372dd01abd82b6d99fab3
Jakub Slominski <u002750@gmail.com>
2019-02-04 19:39:47 +0100

    created repository for clubs

---
+++ System do licencji/Models/ClubRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace System_do_licencji.Models
+{
+    public class ClubRepository : IClubRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public ClubRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Club> GetAllClubs()
+        {
+            return _appDbContext.Clubs ;
+        }
+
+        public Club GetClubInfo(int clubId)
+        {
+            return _appDbContext.Clubs.FirstOrDefault(s => s.Id == clubId);
+        }
+    }
+}
