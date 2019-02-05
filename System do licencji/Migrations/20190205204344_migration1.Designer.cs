3be6f1b54bc87a33baab7e4598ac3a8b7185671d
Jakub Slominski <u002750@gmail.com>
2019-02-05 22:25:27 +0100

    updated migration for database

--- System do licencji/Migrations/20190205204344_migration1.Designer.cs
+++ System do licencji/Migrations/20190205204344_migration1.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using System_do_licencji.Models;
 
 namespace System_do_licencji.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190205204344_migration1")]
+    partial class migration1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ System do licencji/Migrations/20190205204344_migration1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace System_do_licencji.Migrations
+{
+    public partial class migration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clubs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    OwnerId = table.Column<int>(nullable: false),
+                    Address = table.Column<string>(nullable: true),
+                    RegionId = table.Column<int>(nullable: false),
+                    HasFirstLicense = table.Column<bool>(nullable: false),
+                    HasSecondLicense = table.Column<bool>(nullable: false),
+                    HasThirdLicense = table.Column<bool>(nullable: false),
+                    ClubLicenseIsRenewed = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clubs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clubs");
+        }
+    }
+}
