04e933e9cb111218a0cb22dda9830bf3745b12f0
Jakub Slominski <u002750@gmail.com>
2019-06-13 21:58:44 +0200

    profile controler now injects dbcontext and gets user reference for view

--- System do licencji/Controllers/ProfileController.cs
+++ System do licencji/Controllers/ProfileController.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System_do_licencji.Models;
 
 namespace System_do_licencji.Controllers
 {
     public class ProfileController : Controller
     {
+        private readonly UserManager<Player> _userManager;
+
+        public ProfileController(UserManager<Player> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var userId = _userManager.GetUserId(HttpContext.User);
+
+            if (userId == null)
+                return RedirectToAction("Login", "Account");
+            else
+            {
+                Player user = _userManager.FindByIdAsync(userId).Result;
+                return View(user);
+            }
         }
     }
 }
