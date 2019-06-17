1506e8f26a9bad5c622a5dbcaa0a70f736b457f6
Jakub Slominski <u002750@gmail.com>
2019-06-17 19:37:55 +0200

    added edit action to player repostiry model

--- System do licencji/Models/PlayerRepository.cs
+++ System do licencji/Models/PlayerRepository.cs
@@ -23,5 +23,11 @@ namespace System_do_licencji.Models
         {
             return _appDbContext.Players.FirstOrDefault(s => s.Id == playerId);
         }
+
+        public void EdytujSamochod(Player player)
+        {
+            _appDbContext.Players.Update(player);
+            _appDbContext.SaveChanges();
+        }
     }
 }
