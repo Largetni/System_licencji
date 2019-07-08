76df6552654a6e435ea8942a34970491b51cb2f7
Jakub Slominski <u002750@gmail.com>
2019-07-08 19:31:00 +0200

    added http post action to profie controler

--- System do licencji/Controllers/ProfileController.cs
+++ System do licencji/Controllers/ProfileController.cs
@@ -5,12 +5,15 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System_do_licencji.Models;
+using Microsoft.AspNetCore.Http;
+using System_do_licencji.ViewModels;
 
 namespace System_do_licencji.Controllers
 {
     public class ProfileController : Controller
     {
         private readonly UserManager<Player> _userManager;
+        private readonly SignInManager<Player> _signInManager;
 
         public ProfileController(UserManager<Player> userManager)
         {
@@ -30,16 +33,55 @@ namespace System_do_licencji.Controllers
             }
         }
 
+        [HttpGet]
+        public IActionResult Edit(string id)
+        {
+            
+            Player player = _userManager.FindByIdAsync(id).Result;
+            PlayerEditVM playerEditVM=new PlayerEditVM
+            {
+               UserName = player.UserName,
+                Email = player.Email,
+                Name = player.Name,
+                Surname = player.Surname,
+                Street = player.Street,
+                City = player.City
+            };
+
+            return View(playerEditVM);
+        }
+
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(Player player)
+        public IActionResult Edit(PlayerEditVM playerEditVM)
         {
             if (ModelState.IsValid)
             {
-                _userManager.EdytujSamochod(player);
-                return RedirectToAction("Index");
+                Player player = _userManager.GetUserAsync(playerEditVM.);
+
+                var user = new Player()
+                {
+                    UserName = playerEditVM.UserName,
+                    Email = playerEditVM.Email,
+                    Name = playerEditVM.Name,
+                    Surname = playerEditVM.Surname,
+                    Street = playerEditVM.Street,
+                    City = playerEditVM.City,
+                   
+                };
+              //  var result = await _userManager.CreateAsync(user, playerEditVM.Password);
+
+                if (result.Succeeded)
+                {
+
+
+
+
+                    return RedirectToAction("Index", "Profile", new { userID = user.Id });
+                }
             }
-            return View(player);
+            ModelState.AddModelError("", "Edycja nieudana");
+
+            return View(new RegisterVM());
         }
     }
 }
