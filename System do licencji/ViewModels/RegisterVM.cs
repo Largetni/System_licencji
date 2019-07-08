92bd01465681c797ea4fb0fc94593c2cd3995c8b
Jakub Slominski <u002750@gmail.com>
2019-07-08 19:34:45 +0200

    register field for hidden id stump

--- System do licencji/ViewModels/RegisterVM.cs
+++ System do licencji/ViewModels/RegisterVM.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
 
 namespace System_do_licencji.ViewModels
 {
     public class RegisterVM
     {
-      
+
+            [(DisplayValue = false)]
+            public string Id { get; set; }
+
             [Required]
             [EmailAddress]
             [Display(Name = "Email")]
