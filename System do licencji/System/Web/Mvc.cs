37687d71ee73b217f4af3264eee00cbfe4db68b9
Jakub Slominski <u002750@gmail.com>
2019-07-08 19:37:10 +0200

    VM for editing profile added

---
+++ System do licencji/System/Web/Mvc.cs
@@ -0,0 +1,9 @@
+﻿namespace System.Web
+{
+    internal class Mvc
+    {
+        internal class HiddenInputAttribute : Attribute
+        {
+        }
+    }
+}
---
+++ System do licencji/ViewModels/PlayerEditVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace System_do_licencji.ViewModels
+{
+    public class PlayerEditVM :RegisterVM
+    {
+    }
+}
